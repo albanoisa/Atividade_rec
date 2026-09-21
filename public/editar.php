51348806c5ef6dfc2b0f0ada71f326169a43071c
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-09-21 15:37:44 -0300

    add

--- public/editar.php
+++ public/editar.php
@@ -9,3 +9,21 @@ $resultado = mysqli_query($conexao, $sql );
 $brinquedo =mysqli_fetch_assoc($resultado);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - Brinquedos</title>
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>CRUD - Brinquedos</h1>
+    </header>
+    <main>
+        <h2>Editando o brinquedo <?php echo $brinquedo["nome"]?>!</h2>
+        <form action="atualizar.php" method="POST">
