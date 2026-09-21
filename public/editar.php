61a8adf63a123621d9228f145b0721158cdf6a8e
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-09-21 15:39:00 -0300

    editar brinquedos

--- public/editar.php
+++ public/editar.php
@@ -27,3 +27,29 @@ $brinquedo =mysqli_fetch_assoc($resultado);
     <main>
         <h2>Editando o brinquedo <?php echo $brinquedo["nome"]?>!</h2>
         <form action="atualizar.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $brinquedo["id"]?>">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" value="<?php echo $brinquedo["nome"]?>">
+            <br>
+            <label for="categoria">Categoria:</label>
+            <input type="text" name="categoria" value="<?php echo $brinquedo["categoria"]?>">
+            <br>
+            <label for="faixa_etaria">Faixa Etária:</label>
+            <input type="text" name="faixa_etaria" value="<?php echo $brinquedo["faixa_etaria"]?>">
+            <br>
+            <label for="preco">Preço:</label>
+            <input type="number" name="preco" value="<?php echo $brinquedo["preco"]?>" step="0.01">
+            <br>
+            <label for="quantidade">Quantidade:</label>
+            <input type="number" name="quantidade" value="<?php echo $brinquedo["quantidade"]?>">
+            <br>
+            <button type="submit">Atualizar</button>
+        </form>
+
+    </main>
+    <footer>
+    </footer>
+
+
+</body>
+</html>
