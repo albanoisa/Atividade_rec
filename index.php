fdf3054b594b408b99fae3d57c56d8fa55afa9a5
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-09-21 15:33:15 -0300

    index

--- index.php
+++ index.php
@@ -15,3 +15,28 @@ $brinquedos = mysqli_query($conexao, "SELECT * FROM brinquedos");
     <link rel="stylesheet" href="style/styles.css">
 </head>
 
+
+<body>
+    <header>
+        <h1>CRUD - Brinquedos</h1>
+    </header>
+    <main>
+        <h2>Adicione um novo brinquedo!</h2>
+        <form action="public/cadastrar.php" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome">
+            <br>
+            <label for="categoria">Categoria:</label>
+            <input type="text" name="categoria">
+            <br>
+            <label for="faixa_etaria">Faixa Etária:</label>
+            <input type="number" name="faixa_etaria">
+            <br>
+            <label for="preco">Preço:</label>
+            <input type="number" name="preco" step="0.01">
+            <br>
+            <label for="quantidade">Quantidade em estoque:</label>
+            <input type="number" name="quantidade">
+            <br>
+            <button type="submit">Cadastrar</button>
+        </form>
