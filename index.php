09233c2a24f8c032e6a824f61c29a2b9aa0e62b5
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-09-21 15:29:37 -0300

    add

--- index.php
+++ index.php
@@ -4,3 +4,14 @@ include "infra/conexao.php";
 $brinquedos = mysqli_query($conexao, "SELECT * FROM brinquedos");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - Brinquedos</title>
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+
